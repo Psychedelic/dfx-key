23ff4a8b40526c3cebaf28ddcdb27f9134672dff
Scott Dn <scott.dn91@gmail.com>
2022-01-14 06:49:22 +0700

    Ed25519 complete

--- js/ed25519.js
+++ js/ed25519.js
@@ -1,6 +1,8 @@
 import fs from 'fs';
-import {Ed25519KeyIdentity} from '@dfinity/identity';
+import {Ed25519KeyIdentity, Ed25519PublicKey} from '@dfinity/identity';
+import {Principal} from '@dfinity/principal';
 import {toHexString} from './utils.js';
+import tweetnacl from 'tweetnacl';
 
 // ED25519 version 2
 // Private key and public key are included
@@ -11,27 +13,46 @@ export const getIdentityFromPem = (pem) => {
     .replace('\n', '')
     .trim();
 
-  // including padding and pair
-  const raw = Buffer.from(pem, 'base64');
-  console.log('raw: ', raw.toString('hex'));
-
-  const encode = new Uint8Array(raw);
+  const raw = Buffer.from(pem, 'base64')
+    .toString('hex')
+    .replace('3053020101300506032b657004220420', '')
+    .replace('a123032100', '');
 
   // including private key (32 bytes before public key)
   // and public key (last 32 bytes)
-  const key = encode.slice(encode.length - 64, encode.length);
+  const key = new Uint8Array(Buffer.from(raw, 'hex'));
 
   const identity = Ed25519KeyIdentity.fromSecretKey(key);
 
-  // CONFIRM
+  // =============================== CONFIRM ===============================
 
-  console.log('Principal: ', identity.getPrincipal().toText());
+  console.log('Principal:', identity.getPrincipal().toText());
 
   const pair = identity.getKeyPair();
 
-  console.log('Private key: ', toHexString(pair.secretKey));
+  console.log('Private key:', toHexString(pair.secretKey));
 
-  console.log('Public key: ', toHexString(pair.publicKey.toRaw()));
+  console.log('Public key:', toHexString(pair.publicKey.toRaw()));
+
+  console.log('==================================================================');
+
+  const newPair = tweetnacl.sign.keyPair.fromSeed(key.slice(0, 32));
+
+  console.log('New private key:', toHexString(newPair.secretKey));
+
+  console.log('New public key:', toHexString(newPair.publicKey));
+
+  console.log('==================================================================');
+
+  const challenge = 'PUT YOUR MESSAGE HERE';
+  console.log('Message to verify:', challenge);
+
+  const message = new Uint8Array(Buffer.from(challenge, 'utf-8'));
+  const signature = tweetnacl.sign.detached(message, newPair.secretKey);
+  console.log('Signature:', toHexString(signature));
+
+  const result = tweetnacl.sign.detached.verify(message, signature, newPair.publicKey);
+  console.log('Verify signature:', result);
 };
 
 const pem = fs.readFileSync(process.env.PEM_PATH).toString();
