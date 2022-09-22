74c382c4498093117efa1a5f4b04a70850316e16
Scott Dn <scott.dn91@gmail.com>
2022-09-23 04:46:37 +0700

    update cover example

--- cover-sdk.js
+++ cover-sdk.js
@@ -1,6 +1,7 @@
 import fs from "fs";
-import { getPublicKey, sign } from "@psychedelic/cover";
+import { getPublicKey, sign, Cover } from "@psychedelic/cover";
 import { Ed25519KeyIdentity } from "@dfinity/identity";
+import { Principal } from "@dfinity/principal";
 
 const pem = fs
   .readFileSync(process.env.PEM_PATH)
@@ -29,3 +30,53 @@ console.log("timestamp", timestamp);
 
 const signature = await sign(identity, timestamp);
 console.log("signature", signature);
+
+// =========================================================================================================
+// EXAMPLE
+// =========================================================================================================
+const cover = new Cover(identity, { isDevelopment: true });
+
+await cover.deleteBuildConfig(Principal.from("3x7en-uqaaa-aaaai-abgca-cai"));
+
+await cover.saveBuildConfig({
+  canisterId: "3x7en-uqaaa-aaaai-abgca-cai",
+  dfxVersion: "0.11.2",
+  delegateCanisterId: "",
+  canisterName: "cover_test",
+  commitHash: "1423bbf5596263d75fb5414fea45237f9b6ed4f2",
+  repoUrl: "psychedelic/cover",
+  rustVersion: "1.63.0",
+  optimizeCount: 0,
+  repoAccessToken: "",
+});
+
+const buildConfigs = await cover.getBuildConfigs(
+  Principal.from("3x7en-uqaaa-aaaai-abgca-cai")
+);
+console.log("buildConfigs", buildConfigs);
+
+const buildConfig = await cover.getBuildConfigByCanisterId(
+  Principal.from("3x7en-uqaaa-aaaai-abgca-cai")
+);
+console.log("buildConfig", buildConfig);
+
+// ========================================================================================================
+// BUILD
+// ========================================================================================================
+// await cover.buildWithConfig("3x7en-uqaaa-aaaai-abgca-cai");
+
+// await Cover.buildWithCoverMetadata("3x7en-uqaaa-aaaai-abgca-cai", undefined, {
+//   isDevelopment: true,
+// });
+
+await cover.build({
+  canisterId: "3x7en-uqaaa-aaaai-abgca-cai",
+  dfxVersion: "0.11.2",
+  delegateCanisterId: "",
+  canisterName: "cover_test",
+  commitHash: "1423bbf5596263d75fb5414fea45237f9b6ed4f2",
+  repoUrl: "psychedelic/cover",
+  rustVersion: "1.63.0",
+  optimizeCount: 0,
+  repoAccessToken: "",
+});
